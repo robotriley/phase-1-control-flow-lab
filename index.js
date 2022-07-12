2f063aee6befbe2fed8cf06d7074369ee5615acd
riley soloner <rileysoloner@gmail.com>
2022-07-12 10:28:16 -0400

    commit tree ba04bb64cde4e6a6421e32b4eabed342d798e3cd

--- index.js
+++ index.js
@@ -1,11 +1,46 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(distance){
+  if (distance <= 400) {
+    return 'This one is on me!'
+  } else if (distance < 2000) {
+      return 'That will be twenty bucks.'
+  } else if (distance < 2500) {
+    return 'I will gladly take your thirty bucks.'
+  } else if (distance > 2500) {
+    return 'No can do.'
+  }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(destination) {
+
+return destination === "NYC" ? ("Ok, sounds good.") : ("No go.")
 }
 
 function switchOnCharmFromTip(){
   // Write your code here!
-}
+  const expr = 'generous.'
+  switch (expr) {
+    case 'generous':
+      return ('Thank you so much.');
+      break;
+    case 'not as generous':
+      return ('Thank you.');
+      break;
+      default:
+      return ('Bye.');
+  }
+  
+  }
+
+// const expr = 'Papayas';
+// switch (expr) {
+//   case 'Oranges':
+//     console.log('Oranges are $0.59 a pound.');
+//     break;
+//   case 'Mangoes':
+//   case 'Papayas':
+//     console.log('Mangoes and papayas are $2.79 a pound.');
+//     // expected output: "Mangoes and papayas are $2.79 a pound."
+//     break;
+//   default:
+//     console.log(`Sorry, we are out of ${expr}.`);
+// }
